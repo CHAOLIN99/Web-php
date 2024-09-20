7cfff11a819e60da374699bc8591c3e2af58b07f
Wei Cheng <weicheng9722@gmail.com>
2024-09-20 23:33:29 +0000

    clean style.css

--- src/register.php
+++ src/register.php
@@ -1,19 +1,20 @@
 <?php include("head.php"); ?>
 
-<h1>Welcome! Please Register</h1>
-<div>
+
+<div class="center">
+    <p>Welcome! Please Register</p>
     <form action="register.php" method="$_POST">
         <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" placeholder="your first name..."><br>
+        <input type="text" id="fname" name="fname" placeholder="your first name..." class="f_input"><br>
         <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" placeholder="your last name..."><br><br>
+        <input type="text" id="lname" name="lname" placeholder="your last name..." class="f_input"><br><br>
         <label for="email">Email:</label><br>
-        <input type="text" id="email" name="email" placeholder="your email..."><br><br>
+        <input type="text" id="email" name="email" placeholder="your email" class="f_input"><br><br>
         <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" placeholder="set password..."><br><br>
+        <input type="password" id="password" name="password" placeholder="set password" class="f_input"><br><br>
         <label for="cpassword">Confirm Password:</label><br>
-        <input type="password" id="cpassword" name="cpassword" placeholder="please type your password again..."><br><br>
-        <input type="submit" value="Submit">
+        <input type="password" id="cpassword" name="cpassword" placeholder="type your password again" class="f_input"><br><br>
+        <input type="submit" value="Submit" class="submit">
     </form>
 </div>
 
